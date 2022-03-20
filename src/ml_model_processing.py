42a67679a0e0620071e6250b4856c54785429a26
mdjamina <m.djarfi.amina@gmail.com>
2022-03-20 19:41:17 +0100

    add model processing script

--- src/ml_model_processing.py
+++ src/ml_model_processing.py
@@ -1,6 +1,5 @@
 import pandas as pd
-from joblib import Memory
-from shutil import rmtree
+from joblib import Memory, dump
 
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
@@ -8,6 +7,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split 
 from sklearn import metrics
 
+#https://docs.python.org/fr/3/howto/logging.html
+import logging
+
+# Create a temporary folder to store the transformers of the pipeline
+mem = Memory(location="/tmp/cachedir", verbose=0)
 
 def char_tokenizer(text):
   """Character Tokenization splits apiece of text 
@@ -17,56 +21,64 @@ def char_tokenizer(text):
   return [c for c in text if c not in ' \t\n']
 
 
+def get_model(x,y,tokenizer=None,ngram_range=(1,1), verbose=True, memory=None ):
+    model = Pipeline([('TfidfVectorizer', TfidfVectorizer(tokenizer=tokenizer, ngram_range=ngram_range)),
+                     ('LinearSVC', LinearSVC()),],verbose=verbose,memory=memory)
+    model.fit(x, y)
+
+    return model
+    
+
+
 def main():
 
     # data directory
     data_dir = './data/'
 
-    # corpus
+    # pickle dataset path file
     corpus_data_path = data_dir + 'data.pkl'
 
+    # pickle output model path file
+    output_model_path = data_dir + 'lang_detector.joblib'
 
-
-    # Create a temporary folder to store the transformers of the pipeline
-    mem = Memory(location="/tmp/cachedir", verbose=0)
-
- 
-    langs = {'eng': 'English', 'pol': 'Polish', 'deu': 'German', 'fra': 'French', 'spa': 'Spanish', 'ita': 'Italian', 'tur': 'Turkish', 
-             'por': 'Portuguese', 'rus': 'Russian', 'ukr': 'Ukrainian', 'nld': 'Dutch', 'bul': 'Bulgarian', 'ell': 'Greek', 'swe': 'Swedish', 
-             'hun': 'Hungarian', 'gle': 'Irish', 'lav': 'Latvian', 'dan': 'Danish', 'fin': 'Finnish',
-             'ara': 'Arabic', 'heb': 'Hebrew', 'zho': 'Chinese', 'hin': 'Hindi', 'jpn': 'Japanese', 'fas': 'Persian', 'kor': 'Korean',
-             'hye': 'Armenian', 'swa': 'Swahili', 'ber': 'Berber', 'kab': 'Kabyle', 'ces': 'Czech', 'lat': 'Latin',
-             'nor': 'Norwegian', 'ron': 'Moldavian, Moldovan, Romanian', 'slk': 'Slovak', 'hbs': 'Serbo-Croatian', 'mkd': 'Macedonian',
-             'vie': 'Vietnamese', 'est': 'Estonian', 'tha': 'Thai'}
-
-
-
-
+    print("\nLoad pickled dataset from file...")
+    print(corpus_data_path)
+    #Load pickled dataset from file
     data = pd.read_pickle(corpus_data_path) 
+    print(f'data size: {data.shape}')
 
-    test_size= 0.1
-    train_size=0.2
+    print("\nSplit datset into random train and test subsets...")
 
-    x_train, x_test, y_train, y_test = train_test_split(data['content'], data['lang'], test_size=test_size, train_size=train_size, random_state=1) 
-
-
-    model = Pipeline([('TfidfVectorizer', TfidfVectorizer(tokenizer=char_tokenizer, ngram_range=(1,2),)),
-                     ('LinearSVC', LinearSVC()),],verbose=True,memory=mem)
-
-
-    model.fit(x_train, y_train)  
-
-
-    predictions = model.predict(x_test)
-
+    #la taille de dataset test (33%)
+    x_train, x_test, y_train, y_test = train_test_split(data['content'], data['lang'], test_size=0.33,train_size=0.5,  random_state=42) 
+    print(f'x_train size (50%):\n {y_train.value_counts()}')
 
     
-    df_metrics = pd.DataFrame( metrics.classification_report(y_test,predictions, output_dict=True))
+
+    print("\nLinear Support Vector Classification...")
+    model = Pipeline([('TfidfVectorizer', TfidfVectorizer(tokenizer=char_tokenizer, ngram_range=(1,2))),
+                     ('LinearSVC', LinearSVC()),],verbose=True,memory=mem)
+    
+    print("\nFit the model...")
+    model.fit(x_train, y_train)
+
+    #sauvgarde du modèle 
+    print("\nPersist model into file...")
+    dump(model, output_model_path) 
+
+    print(f"File: {output_model_path}")
 
 
-    df_metrics.to_pickle(data_dir +'data_metrics.pkl')
+    #calcul des prédictions
+    print('\npredict...')
+    predictions = model.predict(x_test)
 
+    #df_metrics = 
+    
+    
+    print(metrics.classification_report(y_test,predictions, output_dict=False))
 
+    
 
 
 if __name__ == "__main__":
